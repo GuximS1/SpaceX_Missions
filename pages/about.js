480f8e54c67971692aae7f7313e77f798f7b2d6d
Guxim <guxim.selmani1@hotmail.com>
2021-11-16 17:03:45 +0100

    .

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,10 @@ const About = () => {
     return (
         <div className={styles.homeContent}>
           <h1>About</h1>
-          <p>SpaceX was formed by entrepreneur Elon Musk in the hopes of revolutionizing the aerospace industry and making affordable spaceflight a reality. The company entered the arena with the Falcon 1 rocket, a two-stage liquid-fueled craft designed to send small satellites into orbit!</p>
+          <p>SpaceX was formed by entrepreneur Elon Musk in the hopes of revolutionizing the aerospace industry and making affordable spaceflight a reality. The company entered the arena with the Falcon 1 rocket, a two-stage liquid-fueled craft designed to send small satellites into orbit.</p>
+          <p>SpaceX scored its first big headline in 2010, when it became the first private company to launch a payload into orbit and return it to Earth intact—something only government agencies like NASA or Russia's Roscosmos had done before. Its upright landing and recovery of the first stage of the Falcon 9 rocket on Dec.</p>
+          <p>The point of this website is to show the past launches that SpaceX made and dive right into the details about the rockets.</p>
+          <p>Enjoy it :)</p>
         </div>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,57 @@
 /* eslint-disable */
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import imageSource from "../public/56412099_303.jpg"
+import Image from 'next/image'
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  boldness:{
+    color:'white'
+  }
+};
 
-export default function Home() {
+function Home(props) {
+  const { classes } = props;
   return (
-    <div className={styles.homeContent}>
-    <h1>Homepage</h1>
-          <p>Here u can see the missions of SpaceX!</p>
+  <div>
+    <h1 className={classes.boldness}>SpaceX Past Launches</h1>
+    <Card className={classes.card}>
+      <CardActionArea>
+      <Image
+        alt="Mountains"
+        src={imageSource}
+        objectFit="cover"
+        quality={100}
+      />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Falcon 9
+          </Typography>
+          <Typography component="p">
+          On 24 January 2021, Falcon 9 set a new record for the most satellites launched by a single rocket carrying 143 satellites into orbit.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
     </div>
-    
-  )
+  );
 }
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Home);
